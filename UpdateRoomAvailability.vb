768eed60e67a591e28437173d2d8e0ffda87cb5e
MaheranHafizah <50306577+MaheranHafizah@users.noreply.github.com>
2019-05-08 13:39:18 +0800

    Add files via upload

---
+++ UpdateRoomAvailability.vb
@@ -0,0 +1,55 @@
+﻿'Description: to update room availability
+
+Imports System.Data
+Imports System.Data.SqlClient
+
+Public Class UpdateRoomAvailability
+    Dim con As New SqlConnection
+    Dim da As New SqlDataAdapter
+    Dim com As SqlCommand
+    Dim ds As New DataSet
+    Dim getemp As String
+    Dim dr As SqlDataReader
+    'Function TextBox1_TextChanged() is to fill up the room number that want to be updated
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles text_r_no.TextChanged
+        con = New SqlConnection("Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\MS\Downloads\SEM project\HotelManagementSystemInVB\HotelManagementSystemInVB\HotelManagementSystemInVB\Database1.mdf;Integrated Security=True")
+        con.Open()
+
+        Try
+            getemp = "select r_type, r_rate, no_person, no_bed from room where r_no='" & text_r_no.Text & "'"
+            com = New SqlCommand(getemp, con)
+            dr = com.ExecuteReader()
+            If dr.Read() Then
+                label_r_type.Text = dr.GetValue(0).ToString()
+                label_r_rate.Text = dr.GetValue(1).ToString()
+                label_no_person.Text = dr.GetValue(2).ToString()
+                label_no_bed.Text = dr.GetValue(3).ToString()
+            End If
+        Catch ex As Exception
+            MsgBox(ex.ToString)
+        End Try
+    End Sub
+    'Function Button1_Click() is to update the room into the system
+    'Function ComboBox1.Text is to choose whther the room is available or not into the system
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        con = New SqlConnection("Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\MS\Downloads\SEM project\HotelManagementSystemInVB\HotelManagementSystemInVB\HotelManagementSystemInVB\Database1.mdf;Integrated Security=True")
+        com = New SqlCommand(" update room set r_availability='" + ComboBox1.Text + "' where r_no= '" & text_r_no.Text & "'", con)
+        con.Open()
+        com.ExecuteNonQuery()
+        MsgBox("Check Out Data updated successfully..")
+        Hide()
+        con.Close()
+    End Sub
+    'Function button_reset_Click(): Reset the details that has been filled to the form
+    'textbox and label has changed the name to be more precise
+
+    Private Sub button_reset_Click(sender As Object, e As EventArgs) Handles button_reset.Click
+        text_r_no.Text = ""
+        label_r_type.Text = ""
+        label_r_rate.Text = ""
+        label_no_person.Text = ""
+        label_no_bed.Text = ""
+    End Sub
+End Class
